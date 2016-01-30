59e3ba6329b4454d95e18c4ae5f4285b13870c69
aaron.paul@g.austincc.edu <aaron.paul@g.austincc.edu>
2016-01-29 22:40:43 -0600

    Initial Commit

---
+++ List Implementation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  List Implementation
+//
+//  Created by Aaron on 1/29/16.
+//  Copyright © 2016 Aaron. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
